af2104705ad50e865520d0ba2e487ef05710b519
Md. Yeamin Islam <yeamin15-13529@diu.edu.bd>
2025-02-15 22:12:27 +0600

    2nd code added

---
+++ Day-1/2nd-code.js
@@ -0,0 +1,16 @@
+//This is mathematical expressions
+
+let add, sub, mul, div, value1, value2, value3;
+
+value1 = 100, value2 = 200, value3 = 300;
+
+add = value1 + value2 + value3;
+
+sub = value3 - value2;
+
+mul = value3 * 2;
+
+div = value3 / 3;
+
+console.log(`This is additions result = ${add},\n This is subtraction result = ${sub},\n This is multiplication result = ${mul},\n
+  This is div result =${div}`);
